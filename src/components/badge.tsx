43705ba954061a0cae82a78c942b8393f0b99d1f
szymonrybczak <szymon.rybczak@gmail.com>
2024-01-04 15:22:38 +0100

    feat: add bio

---
+++ src/components/badge.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Badge({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
+    >
+      {children}
+    </Link>
+  );
+}
