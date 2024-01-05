def584ac41597da620fac2b127e87411395311a7
szymonrybczak <szymon.rybczak@gmail.com>
2024-01-05 18:20:29 +0100

    feat: move social media links to nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import Badge from "@/components/badge";
 import Image from "next/image";
 import party from "./../public/images/party.jpg";
@@ -6,8 +5,6 @@ import berlin from "./../public/images/berlin.jpg";
 import camera from "./../public/images/camera.jpg";
 import rneu from "./../public/images/rneu.jpg";
 import festival from "./../public/images/festival.jpg";
-import { ArrowUpRight } from "lucide-react";
-import Followers from "@/components/followers";
 
 export default function Page() {
   return (
@@ -94,68 +91,8 @@ export default function Page() {
         R&D and Open Source initiatives. I{"'"}m writing a lot of technical
         content mainly about React Native and new things that are coming to the
         framework! I am also attending ZSEL 1 High School in Cracow (sometimes
-        😅). You can follow my work on Social Media listed below ✨
+        😅).
       </p>
-      <div className="flex gap-4 pt-6">
-        <div className="group">
-          <a
-            href={"https://twitter.com/SzymonRybczak"}
-            target="_blank"
-            className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 px-3 py-4 dark:border-neutral-700 dark:bg-neutral-800"
-          >
-            <div className="flex items-center space-x-3">
-              <div className="relative h-16">
-                <Image
-                  alt={"Twitter avatar"}
-                  src={"/images/avatar.jpg"}
-                  height={64}
-                  width={64}
-                  className="min-h-[64px] min-w-[64px] rounded-full border border-neutral-200 dark:border-neutral-700"
-                />
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 30 30"
-                  width="240px"
-                  height="240px"
-                  className="relative -right-10 -top-6  h-6 w-6 rounded-full border border-neutral-200 bg-white p-1 dark:border-neutral-700 dark:bg-black dark:fill-white"
-                >
-                  <path d="M26.37,26l-8.795-12.822l0.015,0.012L25.52,4h-2.65l-6.46,7.48L11.28,4H4.33l8.211,11.971L12.54,15.97L3.88,26h2.65 l7.182-8.322L19.42,26H26.37z M10.23,6l12.34,18h-2.1L8.12,6H10.23z" />
-                </svg>
-              </div>
-              <div className="flex flex-col">
-                <p className="font-medium text-neutral-900 dark:text-neutral-100">
-                  @SzymonRybczak
-                </p>
-                <Suspense fallback={<p className="h-6" />}>
-                  <Followers />
-                </Suspense>
-              </div>
-            </div>
-            <div className="hidden transform pl-8 text-neutral-700 transition-transform duration-300 group-hover:-rotate-12 sm:block dark:text-neutral-300">
-              <ArrowUpRight />
-            </div>
-          </a>
-        </div>
-
-        <div>
-          <a
-            href={"https://github.com/SzymonRybczak"}
-            target="_blank"
-            className="flex items-center justify-between rounded border border-neutral-200 bg-neutral-50 p-4 dark:border-neutral-700 dark:bg-neutral-800"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 30 30"
-              width={64}
-              height={64}
-              className="fill-black dark:fill-white"
-            >
-              <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z" />
-            </svg>
-          </a>
-        </div>
-      </div>
     </section>
   );
 }
