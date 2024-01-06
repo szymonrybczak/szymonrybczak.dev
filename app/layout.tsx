88c4118f2980fb933c69d6050b07f38f9a4285be
szymonrybczak <szymon.rybczak@gmail.com>
2024-01-06 19:00:56 +0100

    fix: suppress hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system">
           <main
